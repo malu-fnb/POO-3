0d6612b2729ac59b4490fdb8988fbd4f49d54387
malu-fnb <134156256+malu-fnb@users.noreply.github.com>
2024-06-10 14:53:07 -0300

    Update Labirinto.py

--- Labirinto.py
+++ Labirinto.py
@@ -5,9 +5,9 @@ class Labirinto:
         self.estrutura = [['.' for _ in range(colunas)] for _ in range(linhas)]
         self.tesouros = []
         self.perigos = []
-        self.paredes = []
+        self.pomo_de_ouro = PomoDeOuro(linhas, colunas)
 
-    def gerar_labirinto(self, quantidade_tesouros, quantidade_perigos, quantidade_paredes):
+    def gerar_labirinto(self, quantidade_tesouros, quantidade_perigos):
         for _ in range(quantidade_tesouros):
             while True:
                 linha = random.randint(0, self.linhas - 1)
@@ -26,14 +26,6 @@ class Labirinto:
                     self.adicionar_perigo(perigo)
                     break
 
-        for _ in range(quantidade_paredes):
-            while True:
-                linha = random.randint(0, self.linhas - 1)
-                coluna = random.randint(0, self.colunas - 1)
-                if (linha, coluna) != (0, 0) and self.estrutura[linha][coluna] == '.':
-                    self.adicionar_parede((linha, coluna))
-                    break
-
     def adicionar_tesouro(self, tesouro):
         self.tesouros.append(tesouro)
         linha, coluna = tesouro.get_posicao()
@@ -47,29 +39,38 @@ class Labirinto:
     def adicionar_perigo(self, perigo):
         self.perigos.append(perigo)
         linha, coluna = perigo.get_posicao()
-        self.estrutura[linha][coluna] = '*'
+        self.estrutura[linha][coluna] = '.'  # Esconder armadilhas
 
     def remover_perigo(self, perigo):
         self.perigos.remove(perigo)
         linha, coluna = perigo.get_posicao()
         self.estrutura[linha][coluna] = '.'
 
-    def adicionar_parede(self, posicao):
-        self.paredes.append(posicao)
-        linha, coluna = posicao
-        self.estrutura[linha][coluna] = '-'
-
-    def exibir_mapa(self, aventureiro):
+    def exibir_mapa(self, aventureiro, energia, proximidade_armadilha):
+        clear_terminal()  # Limpa o terminal antes de exibir o mapa
+        print(f"Energia: {energia}, Vidas: {aventureiro.get_vidas()}, {proximidade_armadilha}")
         for i in range(self.linhas):
             for j in range(self.colunas):
                 if (i, j) == aventureiro.get_posicao_atual():
                     print('嘿', end=' ')  # Representa o aventureiro com '嘿'
+                elif (i, j) == self.pomo_de_ouro.get_posicao():
+                    print('@', end=' ')  # Representa o Pomo de Ouro com '@'
                 elif self.estrutura[i][j] == 'T':
                     print('✧', end=' ')  # Representa os tesouros com '✧'
-                elif self.estrutura[i][j] == '*':
-                    print('۝', end=' ')  # Representa as armadilhas com '۝'
-                elif self.estrutura[i][j] == '-':
-                    print('-', end=' ')  # Representa as paredes com '-'
+                elif (i, j) in [p.get_posicao() for p in self.perigos]:
+                    print('A', end=' ')  # Representa as armadilhas com 'A'
                 else:
-                    print('*', end=' ')  # Representa células vazias com '*'
+                    print('.', end=' ')  # Representa células vazias com '.'
             print()
+
+    def verificar_armadilhas_proximas(self, posicao):
+        linha, coluna = posicao
+        proximas_posicoes = [
+            (linha - 1, coluna), (linha + 1, coluna),
+            (linha, coluna - 1), (linha, coluna + 1)
+        ]
+        for pos in proximas_posicoes:
+            if 0 <= pos[0] < self.linhas and 0 <= pos[1] < self.colunas:
+                if pos in [p.get_posicao() for p in self.perigos]:
+                    return "Cuidado, armadilha está próxima!"
+        return "Você está seguro."
